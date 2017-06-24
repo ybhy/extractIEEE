9d77e1d11b22ff8a67c50f0c16a73d63c6c06fd3
ybhy <869118860@qq.com>
2017-06-24 19:04:23 +0800

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
+            this.bindingNavigator1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 26);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(572, 233);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bindingNavigator1
+            // 
+            this.bindingNavigator1.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.bindingNavigator1.CountItem = this.bindingNavigatorCountItem;
+            this.bindingNavigator1.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.bindingNavigator1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem});
+            this.bindingNavigator1.Location = new System.Drawing.Point(0, 0);
+            this.bindingNavigator1.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.bindingNavigator1.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.bindingNavigator1.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.bindingNavigator1.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.bindingNavigator1.Name = "bindingNavigator1";
+            this.bindingNavigator1.PositionItem = this.bindingNavigatorPositionItem;
+            this.bindingNavigator1.Size = new System.Drawing.Size(677, 25);
+            this.bindingNavigator1.TabIndex = 1;
+            this.bindingNavigator1.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "新添";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(32, 22);
+            this.bindingNavigatorCountItem.Text = "/ {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "总项数";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "删除";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "移到第一条记录";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "移到上一条记录";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "位置";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "当前位置";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Text = "移到下一条记录";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Text = "移到最后一条记录";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(590, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bindingNavigator1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
+            this.bindingNavigator1.ResumeLayout(false);
+            this.bindingNavigator1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.BindingNavigator bindingNavigator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public List<string> url = new List<string>();
+        public string urlhead = "http://ieeexplore.ieee.org";
+        public int index = 0;
+
+        public int sum = 1000;
+        public int sumcount = 0;
+        public int indexsum = 0;
+        public int start = 953;
+        public int end = 2700;
+        public Form1()
+        {
+            InitializeComponent();
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Navigate("http://ieeexplore.ieee.org/xpl/RecentIssue.jsp?punumber=79");
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            url.Clear();
+            timer1.Stop();
+            timer2.Stop();
+            //Test();
+        }
+        //public void Test()
+        //{
+        //    LWEntities1 TDB = new LWEntities1();
+        //    HXfinechemicals tab = new HXfinechemicals();
+        //    tab.title = "title";
+        //    tab.abstracts = "abstract";
+        //    tab.keywords = "keywords";
+        //    TDB.HXfinechemicals.Add(tab);
+        //    TDB.SaveChanges();
+        //}
+        private void timer1_Start(object sender, EventArgs e)
+        {
+            foreach (HtmlElement ahref in this.webBrowser1.Document.GetElementsByTagName("A"))
+            {
+                // Console.WriteLine(ahref.InnerText);
+            }
+            if (index < url.Count)
+            {
+                Console.WriteLine(index + 1);
+                this.webBrowser1.Navigate(url[index]);
+                Extraction(this.webBrowser1.Document);
+                index++;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            using (LWEntities7 TDB = new LWEntities7())
+            {
+                IQueryable<TXsignalprocessingurl> ieurl = from c in TDB.TXsignalprocessingurl select c;
+                //int count = 0;
+                this.timer2.Start();
+                sumcount = ieurl.Count();
+                int indexiiiiii = 0;
+                foreach (var c in ieurl)
+                {
+                    if (indexiiiiii >= start && indexiiiiii < end)
+                        url.Add(c.url);
+                    indexiiiiii++;
+                }
+            }
+        }
+        private void Extraction(HtmlDocument document)
+        {
+            LWEntities7 TDB = new LWEntities7();
+            TXsignalprocessing tab = new TXsignalprocessing();
+            string abstracts = "";
+            string keywords = "";
+            string title = "";
+            string inspec = "";
+            string inspecnot = "";
+            string ieeeterms = "";
+            foreach (HtmlElement div in document.GetElementsByTagName("div"))
+            {
+
+                if (div.GetAttribute("className").Equals("title"))
+                {
+                    //Console.WriteLine("title: "+div.InnerText);
+                    title = div.InnerText;
+                    Console.WriteLine("title: " + title);
+                    //tab.title = title;
+                }
+                if (div.GetAttribute("data-section").Equals("articleDetails.ajax"))
+                {
+                    foreach (HtmlElement div1 in div.GetElementsByTagName("div"))
+                    {
+                        if (div1.GetAttribute("className").Equals("article"))
+                        {
+                            abstracts = div1.InnerText;
+                            Console.WriteLine("abstract: " + div1.InnerText);
+                            //tab.abstracts = div1.InnerText;
+                        }
+                    }
+                }
+                if (div.GetAttribute("data-section").Equals("abstractKeywords.ajax"))
+                {
+                    foreach (HtmlElement div1 in div.GetElementsByTagName("div"))
+                    {
+                        if (div1.GetAttribute("className").Equals("section"))
+                        {
+                            Boolean isAuthorKeywords = false;
+                            foreach (HtmlElement h2 in div1.GetElementsByTagName("h2"))
+                            {
+                                if (h2.InnerText.Equals("AUTHOR KEYWORDS"))
+                                {
+                                    isAuthorKeywords = true;
+                                    break;
+                                }
+                            }
+                            if (isAuthorKeywords)
+                            {
+                                foreach (HtmlElement li in div1.GetElementsByTagName("li"))
+                                {
+                                    keywords += li.InnerText + ";";
+                                }
+                            }
+
+                            Boolean isInspec = false;
+                            foreach (HtmlElement h2 in div1.GetElementsByTagName("h2"))
+                            {
+                                if (h2.InnerText.Equals("INSPEC: CONTROLLED INDEXING"))
+                                {
+                                    isInspec = true;
+                                    break;
+                                }
+                            }
+                            if (isInspec)
+                            {
+                                foreach (HtmlElement li in div1.GetElementsByTagName("li"))
+                                {
+                                    inspec += li.InnerText + ";";
+                                }
+                            }
+
+                            Boolean isInspecNot = false;
+                            foreach (HtmlElement h2 in div1.GetElementsByTagName("h2"))
+                            {
+                                if (h2.InnerText.Equals("INSPEC: NON CONTROLLED INDEXING"))
+                                {
+                                    isInspecNot = true;
+                                    break;
+                                }
+                            }
+                            if (isInspecNot)
+                            {
+                                foreach (HtmlElement li in div1.GetElementsByTagName("li"))
+                                {
+                                    inspecnot += li.InnerText + ";";
+                                }
+                            }
+
+                            Boolean isIeeeTerms = false;
+                            foreach (HtmlElement h2 in div1.GetElementsByTagName("h2"))
+                            {
+                                if (h2.InnerText.Equals("IEEE TERMS"))
+                                {
+                                    isIeeeTerms = true;
+                                    break;
+                                }
+                            }
+                            if (isIeeeTerms)
+                            {
+                                foreach (HtmlElement li in div1.GetElementsByTagName("li"))
+                                {
+                                    ieeeterms += li.InnerText + ";";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            tab.abstracts = abstracts;
+            tab.title = title;
+            tab.keywords = keywords;
+            tab.inspec = inspec;
+            tab.inspecnot = inspecnot;
+            tab.ieeeterms = ieeeterms;
+            tab.label = "Math";
+            tab.search = "function theory";
+            TDB.TXsignalprocessing.Add(tab);
+            TDB.SaveChanges();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Console.WriteLine("sum : " + sum);
+            foreach (HtmlElement ahref in this.webBrowser1.Document.GetElementsByTagName("A"))
+            {
+                if (ahref.GetAttribute("className").Equals("ng-binding ng-scope"))
+                {
+                    string urll = urlhead + ahref.GetAttribute("href");
+                    if (!(urll.Contains("mostRecentIssue")))
+                    {
+                        LWEntities7 TDB = new LWEntities7();
+                        TXsignalprocessingurl tab = new TXsignalprocessingurl();
+                        tab.url = urll;
+                        TDB.TXsignalprocessingurl.Add(tab);
+                        TDB.SaveChanges();
+                    }
+
+                }
+                if (ahref.GetAttribute("ng-click").Equals("selectPage(page + 1)"))
+                {
+                    sum--;
+                    if (sum <= 0)
+                    {
+                        this.timer1.Stop();
+                        //this.timer2.Start();
+                        Console.WriteLine(url.Count);
+                    }
+                    ahref.InvokeMember("click");
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (sumcount >= indexsum)
+            {
+                Console.WriteLine("index : " + indexsum);
+                this.webBrowser1.Navigate(url[indexsum]);
+                Extraction(this.webBrowser1.Document);
+                indexsum++;
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
